80cd2bb5a48cff177c8ea7e78ae0c66a211889c1
davirrocha <davirochamoraes7@gmail.com>
2024-10-13 18:58:47 -0300

    changes-styles

--- document.js
+++ document.js
@@ -1,29 +1,24 @@
 
-
-
 let click = document.querySelector(".sortear")
 
-
-function clickButton() {
-
+click.addEventListener('click', () => {
     const min = Math.ceil(document.querySelector(".left").value)
     const max = Math.floor(document.querySelector(".right").value)
-    const number = document.querySelector(".result")
-    const result = Math.floor(Math.random() * (max - min) + min);
 
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+
+    const number = document.querySelector(".result")
     number.innerHTML = result
 
     if (max >= min) {
         console.log("valor correto")
-        number.style.color = "#a486b0"
+        number.style.color = "#D19E9A"
     } else {
         number.innerHTML = "ERRO!! O valor MÍNIMO tem que ser MENOR que o valor MÁXIMO "
-        number.style.color = "red"
+        number.style.color = "#820B3B"
     }
+})
 
-}
-
-click.addEventListener("click", clickButton)
 
 
 
